8f775bfe0ce831873ca5d41a32b024eb8b8ca480
aayush-wiz <aayush.2791g@gmail.com>
2025-06-26 13:41:29 +0530

    Pushing to frontend

--- src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-  <footer className="py-8 bg-transparent">
-    <div className="container mx-auto px-6 text-center text-slate-400">
-      <p>&copy; {new Date().getFullYear()} Oracyn. All rights reserved.</p>
-    </div>
-  </footer>
-);
-
-export default Footer;
